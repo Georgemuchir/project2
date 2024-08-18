e5ca9879dde7c6985820b2c4e2c232db2ad6e8b9
Georgemuchiri <georgw.muchiri@student.moringaschool.com>
2024-08-18 23:49:45 +0300

    Finised product

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Nav from "./components/Nav";
+import Homepage from "./components/Homepage";
+import Form from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [search, setSearch] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<nav>
+				<Nav search={search} setSearch={setSearch} />
+			</nav>
+			<main>
+				<Homepage search={search} />
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
